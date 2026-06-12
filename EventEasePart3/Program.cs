eb5a04299e9f0fc5551c39c7da1ac3423188fc72
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2026-06-12 15:42:31 +0200

    update

--- EventEasePart3/Program.cs
+++ EventEasePart3/Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseStaticFiles();
